38a88d843baaee852b03b7f562c2bf9f33a28c07
ilazarte <ivan.lazarte@gmail.com>
2016-03-21 16:13:27 -0400

    initial commit

--- app/src/main/assets/query.sql
+++
@@ -1,7 +0,0 @@
--- @security.watchlistid
-
-select *
-from   security
-where  symbol in (select security_symbol
-                   from   watchlist_security
-                   where  watchlist_id = ?)
